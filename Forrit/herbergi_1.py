0e2a721e47707ecef44630b96d79dc6d5096457f
Leikjaland <36199860+Leikjaland@users.noreply.github.com>
2018-02-23 11:35:49 +0000

    Útgáfa 1 , herbergi 1

---
+++ Forrit/herbergi_1.py
@@ -0,0 +1,18 @@
+# Útgáfa 1.0
+# Hanna herbergið með því í huga að það sé hægt að fara á milli herbergja og hægt sé að koma aftur í herbergið.
+# Val á herbergjum fer eftir því hvaða föt eru valin en síðasta herbergið er lokað þangað til að er búið að safna úr herbergi 2 og 3.
+
+
+# Hanna server í herbergið svo hægt sé að fara á milli herbegja.
+
+# Hann svo það sé hægt að taka á móti leikmanni og einning þarf að vita hvaða kyn hefur verið valið.
+
+
+# Hanna svo það sé hægt að fara í herbergi 2 (kennslustofa) með því í huga að leikmaðurinn þarf að velja skólaföt til að far í herbergi 2.
+
+
+# Hanna svo það sé hægt að fara í herbergi 3 (Útivera) með því í huga að leikmaðurinn þarf að velja útiföt til að far í herbergi 3.
+
+
+# Hanna svo það sé hægt að fara í herbergi 4 með því að það er búið að fá báða hlutina úr herberi 2 og 3.
+
