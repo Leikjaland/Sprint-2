b21cce8520cecc397c5df068ec42a36c37543047
Leikjaland <36199860+Leikjaland@users.noreply.github.com>
2018-02-27 17:06:46 +0000

    Útgáfa 1.0

--- Forrit/herbergi_1.py
+++ Forrit/herbergi_1.py
@@ -1,18 +1,74 @@
 # Útgáfa 1.0
-# Hanna herbergið með því í huga að það sé hægt að fara á milli herbergja og hægt sé að koma aftur í herbergið.
-# Val á herbergjum fer eftir því hvaða föt eru valin en síðasta herbergið er lokað þangað til að er búið að safna úr herbergi 2 og 3.
+
+import sys
+import time
+import select
+from herbergi_2 import skolastofa
+
+# Hlutir sem þarf að satna í skólasofu og útiveru
+#bok = {}
+#blom = {}
 
 
-# Hanna server í herbergið svo hægt sé að fara á milli herbegja.
+class leikjaland(object):
 
-# Hann svo það sé hægt að taka á móti leikmanni og einning þarf að vita hvaða kyn hefur verið valið.
-
-
-# Hanna svo það sé hægt að fara í herbergi 2 (kennslustofa) með því í huga að leikmaðurinn þarf að velja skólaföt til að far í herbergi 2.
-
-
-# Hanna svo það sé hægt að fara í herbergi 3 (Útivera) með því í huga að leikmaðurinn þarf að velja útiföt til að far í herbergi 3.
+    #def __init__(self,bok,blom):
+    #    self.bok=bok
+    #    self.blom=blom
 
 
-# Hanna svo það sé hægt að fara í herbergi 4 með því að það er búið að fá báða hlutina úr herberi 2 og 3.
+# Hér ver verið að taka á móti leikmanni og spyrja um kyn.
+    print('Velkomin\\nn í leikjaland')
+    kyn = input('Veldu kyn, strakur eða stelpa: ')
+    print('Þú ert ' + kyn  )
 
+# Hér byrjar herbergi_1
+    def Herbergi_1(self):
+        print('test1')
+        while (1):
+            try:
+                fot= input('Viltu fara í skólann eða út að leika, velja\\skrifa skolafot eða utifot: ')
+                fot=fot
+                break
+# Þarf að skoða betur!
+            except:
+                print ('Þú hefur ekki valið föt')
+
+
+# leikmaður fer í skolastofuna
+        if fot == 'skolafot':
+            print('Þú ert komin\\nn í skólann')
+            #skolastofa(bok)
+            # forritið sem verður fyrir skólastofu herbergið
+
+
+# Leikmaður fer í útiveru
+        elif fot == 'utifot':
+            print('Þú ert komin\\nn út')
+        # utivera(blom)
+        # forritið sem verður fyrir utiveru herbergið
+
+# ef ekki ekki er valið skólaföt eða útiföt
+        else:
+            print ('Þú hefur ekki valið föt')
+            self.Herbergi_1()
+
+# Fallið leyfir leikmanni að koma til baka og skipta um föt til að fara í hitt herbergið
+    def skipta_um_fot(self):
+        if fot == 'skolafot':
+            print('Þú ert að fara í útiföt')
+            # utivera()
+
+        elif fot == 'utifot':
+            print('Þú ert að fara í skólaföt')
+            # skolastofa()
+
+
+
+def  main():
+    leikur=leikjaland()
+    leikur.Herbergi_1()
+
+
+if __name__  == '__main__':
+    main()
